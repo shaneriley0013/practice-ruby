95e77cb10bfc16eedf31adfa3df0e7966cd4daf7
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-21 14:32:04 -0500

    add hashes to file

---
+++ store_item.rb
@@ -0,0 +1,12 @@
+
+
+
+
+ball_1 ={:color => "black", :price => 6, :style => "8 ball" }
+
+ball_2 ={:color => "white", :price => 8, :style => "Cue ball" }
+
+
+ball_3 ={:color => "yellow", :price => 8, :style => "Cue ball" }
+
+p ball_1[:color]
