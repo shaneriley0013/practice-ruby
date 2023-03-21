0afe038956b05158ef766154599903d4d20fb486
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-21 14:49:10 -0500

    add fancy syntax version of hash

--- store_item.rb
+++ store_item.rb
@@ -2,11 +2,20 @@
 
 
 
-ball_1 ={:color => "black", :price => 6, :style => "8 ball" }
+# ball_1 ={:color => "black", :price => 6, :style => "8 ball" }
 
-ball_2 ={:color => "white", :price => 8, :style => "Cue ball" }
+# ball_2 ={:color => "white", :price => 8, :style => "Cue ball" }
 
 
-ball_3 ={:color => "yellow", :price => 8, :style => "Cue ball" }
+# ball_3 ={:color => "yellow", :price => 8, :style => "Cue ball" }
+
+# p ball_1[:color]
+
+ball_1 ={color: "black", price: 6, style: "8 ball" }
+
+ball_2 ={color: "white", price: 8, style: "Cue ball" }
+
+
+ball_3 ={color: "yellow", price: 8, style: "Cue ball" }
 
 p ball_1[:color]
