10120c77e8dfadc379dc3b9881e32b0382af1080
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-24 09:14:08 -0500

    add 2 files

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,3 +1,8 @@
+
+
+
+
+
 class Car
   def initialize
     @speed = 0
@@ -21,25 +26,23 @@ class Car
   end
 end
 
-class Bike
-  def initialize
-    @speed = 0
-    @direction = 'north'
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Car
   def ring_bell
     puts "Ring ring!"
   end
 end
+
+
+bike = Bike.new
+car = Car.new
+
+66.times do
+puts car.accelerate
+end
+
+
+
+# def initialize(input_options)
+#   super
+#   @employees = input_options[:employees]
+# end
